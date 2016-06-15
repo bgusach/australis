0e1d422c8f0c75860d46b2fdc099d02409dc59db
ikaros45 <bgusach@gmail.com>
2016-06-15 22:46:57 +0200

    Added mix example

---
+++ examples/example3.css.js
@@ -0,0 +1,24 @@
+const mix = require('sierra').tools.mix
+
+const square = {
+    width: '50px',
+    height: '50px',
+}
+
+const bordered = {
+    borderStyle: 'solid',
+    borderWidth: '3px',
+    borderColor: 'blue',
+}
+
+
+exports.default = {
+    '.class1': mix(square, bordered, {
+        backgroundColor: 'red',
+    }),
+
+    '.class2': mix(square, {
+        position: 'float',
+        padding: '1px 1px 3px 10px',
+    })
+}
